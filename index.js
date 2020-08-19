31c5ab999798688f0d1613edd20e4d694e8fa9e4
MatheusFr27 <matheusefranca1727@gmail.com>
2020-08-19 13:04:26 -0300

    Adicionando index.js e configurando dependencias

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3000
+
+// Configurar Body-parser
+
+// Inserir / no endpoint
+
+// Inserir caminho coringa
+
+// Iniciar servidor na porta respectiva

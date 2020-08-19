959b4301844179b5723d77d2f5991c7259aa4193
MatheusFr27 <matheusefranca1727@gmail.com>
2020-08-19 13:27:50 -0300

    Inserindo configuração de cabeçalho e resposta da API

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 
 // Configurar Body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json())
+app.use(bodyParser.json({ type: 'application/json' }))
+
+// configurando Cors
+app.use(cors())
+
+// configurando cabeçalho de resposta
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requestred-With, Content-Type, Accept')
+    next()
+})
 
 // Inserir / no endpoint
+app.get('/', (req, res) => {
+    res.send({ message: `API atendendo na porta ${PORT}.`})
+})
 
 // Inserir caminho coringa
+app.use('*', (req, res) => {
+    res.send({ message: 'Caminho não encontrado.'})
+})
 
 // Iniciar servidor na porta respectiva
+app.listen(PORT, () => {
+    console.log(`API atendendo na porta ${PORT}.`)
+})

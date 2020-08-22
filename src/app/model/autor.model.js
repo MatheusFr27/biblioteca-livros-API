1adf4915e796c5b7a8210e6605f845ba06220be2
MatheusFr27 <matheusefranca1727@gmail.com>
2020-08-22 15:51:31 -0300

    Criação Modelo do Autor

---
+++ src/app/model/autor.model.js
@@ -0,0 +1,24 @@
+// Importações
+const {Schema, model} = require('mongoose')
+
+// Modelo
+const ModeloAutor = new Schema({
+
+    nome: {
+        type: String,
+        required: true,
+        maxlength: 51,
+        trim: true
+    },
+    nascimento: {
+        type: Date,
+        required: false,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+module.exports = module('modeloautor', ModeloAutor)

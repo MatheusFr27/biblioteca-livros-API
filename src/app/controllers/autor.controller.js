3ffebab2391e4f832ece096ce097e3aaab268987
MatheusFr27 <matheusefranca1727@gmail.com>
2020-08-22 16:37:27 -0300

    Acrescentando Controller e 2 opções

---
+++ src/app/controllers/autor.controller.js
@@ -0,0 +1,45 @@
+const modeloautor = require('../model/autor.model')
+
+class Autor {
+
+    criarAutor (req, res) {
+        const body = req.body
+
+        modeloautor.create(body, (err, data) => {
+            
+            if (err) {
+                res.status(500).send({ message: 'Houve um problema ao criar o autor.', error: err })
+            } else {
+                res.status(200).send({ message: 'Autor criado com sucesso!', autor: data})
+            }
+        })
+    }
+
+    visualizarTodosAutores (req, res) {
+
+        modeloautor.find({}, (err, data) => {
+
+            if (err) {
+                res.status(500).send({ message: 'Houve um erro ao procurar pelos Autores', error: err })
+            } else {
+                res.status(200).send({ message: 'Autores encontrados com sucesso!', autor: data })
+            }
+        })
+    }
+
+    visualizarUmAutor (req, res) {
+        const nome = req.params.nome
+
+        modeloautor.find({nome: nome}, (err, data) => {
+
+            if (err) {
+                res.status(500).send({ message: 'Houve um erro ao procurar pelo autor', error: err })
+            } else {
+                res.status(200).send({ message: 'Autor encontrado com sucesso.', autor: data })
+            }
+        })
+    }
+
+}
+
+module.exports = new Autor()
